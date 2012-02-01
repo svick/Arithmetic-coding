4ee07aecdd9e6b7c1e6df29125635d862e119394
Petr Onderka <gsvick@gmail.com>
2012-02-01 18:56:06 +0100

    project skeleton

---
+++ ArithmeticCoding.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArithmeticCoding.Console
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                ShowHelp();
+                return;
+            }
+
+            switch (args[0])
+            {
+            case "-c":
+            case "c":
+                Compress(args[1], args[2]);
+                break;
+            case "-d":
+            case "d":
+                Decompress(args[1], args[2]);
+                break;
+            default:
+                ShowHelp();
+                break;
+            }
+        }
+
+        private static void ShowHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Compress(string inputFile, string outputFile)
+        {
+            ArithmeticCoding.Compress(inputFile, outputFile);
+        }
+
+        private static void Decompress(string inputFile, string outputFile)
+        {
+            ArithmeticCoding.Decompress(inputFile, outputFile);
+        }
+    }
+}
---
+++ ArithmeticCoding/ArithmeticCoding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArithmeticCoding
+{
+    public static class ArithmeticCoding
+    {
+        public static void Compress(string inputFile, string outputFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Decompress(string inputFile, string outputFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

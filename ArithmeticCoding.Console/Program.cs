489c0bc77bee85bda14d0caf398e97cea97bdd93
Petr Onderka <gsvick@gmail.com>
2012-02-05 20:14:49 +0100

    added CLI

--- ArithmeticCoding.Console/Program.cs
+++ ArithmeticCoding.Console/Program.cs
@@ -1,36 +1,63 @@
 ﻿using System;
+using System.IO;
+using log4net;
+using log4net.Config;
 
 namespace ArithmeticCoding.Console
 {
     static class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
-            if (args.Length != 3)
+            try
             {
-                ShowHelp();
-                return;
-            }
+                SetupLogging();
 
-            switch (args[0])
-            {
-            case "-c":
-            case "c":
-                Compress(args[1], args[2]);
-                break;
-            case "-d":
-            case "d":
-                Decompress(args[1], args[2]);
-                break;
-            default:
-                ShowHelp();
-                break;
+                if (args.Length != 3)
+                {
+                    ShowHelp();
+                    return;
+                }
+
+                switch (args[0])
+                {
+                case "-c":
+                case "c":
+                    Compress(args[1], args[2]);
+                    break;
+                case "-d":
+                case "d":
+                    Decompress(args[1], args[2]);
+                    break;
+                default:
+                    ShowHelp();
+                    break;
+                }
             }
+            catch (Exception e)
+            {
+                Log.Error(e);
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void SetupLogging()
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
         }
 
         private static void ShowHelp()
         {
-            throw new NotImplementedException();
+            string fileName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+
+            System.Console.WriteLine("Usage: {0} -c input output", fileName);
+            System.Console.WriteLine("       {0} -d input output", fileName);
+            System.Console.WriteLine();
+            System.Console.WriteLine("Use -c to compress input into output.");
+            System.Console.WriteLine("Use -d to decompress input into output.");
+            System.Console.WriteLine("Input and output are paths to the corresponding files.");
         }
 
         private static void Compress(string inputFile, string outputFile)
